bd72f111d11ec97601b53212f327b90d5e2b518c
wyao <yaowen1612@sina.com>
2017-05-28 15:54:44 +0800

    创建工程

---
+++ iOS_demo/iOS_demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS_demo
+//
+//  Created by wyao on 2017/5/28.
+//  Copyright © 2017年 wyao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS_demo/iOS_demo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  iOS_demo
+//
+//  Created by wyao on 2017/5/28.
+//  Copyright © 2017年 wyao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
