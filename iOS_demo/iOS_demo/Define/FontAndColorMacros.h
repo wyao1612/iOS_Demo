d8640172784239df2a52934a050d05c0282f0938
wyao <yaowen1612@sina.com>
2017-06-21 18:29:27 +0800

    基础文件夹搭建

---
+++ iOS_demo/iOS_demo/Define/FontAndColorMacros.h
@@ -0,0 +1,14 @@
+//
+//  FontAndColorMacros.h
+//  iOS_demo
+//
+//  Created by wyao on 2017/6/20.
+//  Copyright © 2017年 wyao. All rights reserved.
+//
+
+//定义全局用的色值、字体大小，这里建议跟设计师共同维护一个设计规范
+#ifndef FontAndColorMacros_h
+#define FontAndColorMacros_h
+
+
+#endif /* FontAndColorMacros_h */
---
+++ test/test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test
+//
+//  Created by wyao on 2017/6/15.
+//  Copyright © 2017年 wyao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
