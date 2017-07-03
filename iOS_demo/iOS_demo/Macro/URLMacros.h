50a1cb4927ff28c56a5c69d772f33a451171e5d0
wyao <yaowen1612@sina.com>
2017-07-03 09:41:15 +0800

    增加基础框架结构

---
+++ iOS_demo/iOS_demo/Macro/URLMacros.h
@@ -0,0 +1,15 @@
+//
+//  URLMacros.h
+//  iOS_demo
+//
+//  Created by wyao on 2017/6/20.
+//  Copyright © 2017年 wyao. All rights reserved.
+//
+
+
+//定义服务器接口地址以及环境开关；
+#ifndef URLMacros_h
+#define URLMacros_h
+
+
+#endif /* URLMacros_h */
